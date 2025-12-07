15600ad8ac0377665bd555a3186493e9c1423028
김은성 <kimsnfnr@gmail.com>
2025-12-07 23:44:21 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,14 @@
 
 # 경로 설정
 BASE_PATH = "HarryPotter"
-RAW_FILENAMES = ["01 Harry Potter and the Sorcerers Stone.txt"]
+MERGED_FILENAME = "combined_harry_potter.txt"
 
-# 하이퍼파라미터 설정 (CPU 환경 고려)
-BATCH_SIZE = 32
-BLOCK_SIZE = 64      # 문맥 길이 (Context Window)
-EMB_DIM = 256        # 임베딩 차원
-N_HEAD = 4           # Attention Head 수
-N_LAYER = 4          # Transformer Layer 수
-LEARNING_RATE = 5e-4 
-EPOCHS = 3           # Loss 변화 그래프 확인용
+# 하이퍼파라미터 설정
+BATCH_SIZE = 64
+BLOCK_SIZE = 256     # 문맥 길이 (Context Window)
+EMB_DIM = 384        # 임베딩 차원
+N_HEAD = 6           # Attention Head 수
+N_LAYER = 6          # Transformer Layer 수
+LEARNING_RATE = 3e-4 
+
+EPOCHS = 10         
